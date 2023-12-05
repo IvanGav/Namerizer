27c076d41719375582f275bbfdb014b3ef9b7502
Angel Martinez <Martinezangel3698@gmail.com>
2023-12-05 11:45:23 -0800

    upddated web

--- namerizer/lib/android_source/android.dart
+++ namerizer/lib/android_source/android.dart
@@ -1,6 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:flutter/services.dart';
-
 
 import "home.dart";
 import "login.dart";
--- namerizer/lib/web_source/web.dart
+++ namerizer/lib/web_source/web.dart
@@ -1,5 +1,6 @@
 import "package:google_fonts/google_fonts.dart";
 import "package:image_picker/image_picker.dart";
+import 'package:flutter/services.dart';
 import "package:flutter/material.dart";
 import "package:camera/camera.dart";
 
